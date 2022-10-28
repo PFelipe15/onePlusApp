53412695cf8ed98180a71e0f44091581d7cef65e
Paulo Felipe <PauloFelipet15@gmail.com>
2022-10-28 12:54:02 -0300

    Desmembrando components

---
+++ src/Screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text } from "react-native";
+import Background from "../components/Background";
+import Main from "../components/MainView";
+
+
+
+export default function LoginScreen() {
+  return (
+    <Background>
+      <Main>
+        <Text>Ola</Text>
+      </Main>
+    </Background>
+  );
+}
